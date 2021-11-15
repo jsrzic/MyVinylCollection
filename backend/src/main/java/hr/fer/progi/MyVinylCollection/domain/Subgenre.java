5fb8c73b9a89e4665ef94ac34a5b8cb5dfdb7209
Marta <marta.dulibic@fer.hr>
2021-11-15 22:56:41 +0100

    Refactor genre/subgenre logic

--- backend/src/main/java/hr/fer/progi/MyVinylCollection/domain/Subgenre.java
+++ backend/src/main/java/hr/fer/progi/MyVinylCollection/domain/Subgenre.java
@@ -1,2 +1,41 @@
-package hr.fer.progi.MyVinylCollection.domain;public class Subgenre {
+package hr.fer.progi.MyVinylCollection.domain;
+
+import javax.persistence.*;
+
+@Entity(name="subgenres")
+public class Subgenre {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name="parent_genre_id", nullable=false)
+    private Genre genre;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
