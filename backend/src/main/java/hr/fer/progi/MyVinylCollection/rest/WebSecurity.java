2808aca77306c8c122c0907268f0882cb0794619
Marta <marta.dulibic@fer.hr>
2021-11-17 11:45:40 +0100

    Start cookie setup

--- backend/src/main/java/hr/fer/progi/MyVinylCollection/rest/WebSecurity.java
+++ backend/src/main/java/hr/fer/progi/MyVinylCollection/rest/WebSecurity.java
@@ -1,14 +1,28 @@
 package hr.fer.progi.MyVinylCollection.rest;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("adminPass")).roles("ADMIN");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
