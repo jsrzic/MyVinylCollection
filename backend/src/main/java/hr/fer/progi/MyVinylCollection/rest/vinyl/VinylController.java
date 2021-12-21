1979404a43c88908669e13cbbdb6b43fe1231b6b
Tea Krišto <tea.kristo@fer.hr>
2021-12-21 17:38:59 +0100

    Add endpoints for delete and update vinyl.

--- backend/src/main/java/hr/fer/progi/MyVinylCollection/rest/vinyl/VinylController.java
+++ backend/src/main/java/hr/fer/progi/MyVinylCollection/rest/vinyl/VinylController.java
@@ -1,19 +1,17 @@
 package hr.fer.progi.MyVinylCollection.rest.vinyl;
 
 
-import hr.fer.progi.MyVinylCollection.domain.Genre;
 import hr.fer.progi.MyVinylCollection.domain.User;
 import hr.fer.progi.MyVinylCollection.domain.Vinyl;
 import hr.fer.progi.MyVinylCollection.rest.security.VinylUserDetails;
-import hr.fer.progi.MyVinylCollection.rest.user.dto.RegisterUserDTO;
+import hr.fer.progi.MyVinylCollection.rest.vinyl.dto.UpdateVinylDTO;
 import hr.fer.progi.MyVinylCollection.service.UserService;
 import hr.fer.progi.MyVinylCollection.service.VinylService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
@@ -34,5 +32,24 @@ public class VinylController {
         System.out.println(userDetails.getUsername());
         User user = userService.findByUsername(userDetails.getUsername());
         return vinylService.addVinyl(vinyl, user);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Object> deleteVinyl(@PathVariable Long id){
+        if(vinylService.deleteVinyl(id)){
+            return new ResponseEntity<Object>(id, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<Object>(id, HttpStatus.EXPECTATION_FAILED);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateVinylInfo(@PathVariable Long id,
+                                              @RequestBody UpdateVinylDTO updatedVinyl){
+        if(vinylService.updateVinylInfo(id, updatedVinyl)){
+            return new ResponseEntity<Object>(id, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<Object>(id, HttpStatus.EXPECTATION_FAILED);
+        }
     }
 }
--- backend/src/main/java/hr/fer/progi/MyVinylCollection/service/impl/VinylServiceJpa.java
+++ backend/src/main/java/hr/fer/progi/MyVinylCollection/service/impl/VinylServiceJpa.java
@@ -3,6 +3,9 @@ package hr.fer.progi.MyVinylCollection.service.impl;
 import hr.fer.progi.MyVinylCollection.dao.VinylRepository;
 import hr.fer.progi.MyVinylCollection.domain.User;
 import hr.fer.progi.MyVinylCollection.domain.Vinyl;
+import hr.fer.progi.MyVinylCollection.mapper.MapStructMapper;
+import hr.fer.progi.MyVinylCollection.rest.vinyl.dto.UpdateVinylDTO;
+import hr.fer.progi.MyVinylCollection.service.RequestDeniedException;
 import hr.fer.progi.MyVinylCollection.service.VinylService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +16,9 @@ public class VinylServiceJpa implements VinylService {
     @Autowired
     private VinylRepository vinylRepo;
 
+    @Autowired
+    private MapStructMapper mapstructMapper;
+
 
     @Override
     public Vinyl addVinyl(Vinyl vinyl, User user) {
@@ -21,12 +27,22 @@ public class VinylServiceJpa implements VinylService {
     }
 
     @Override
-    public boolean updateVinylInfo(Vinyl vinyl) {
-        return false;
+    public boolean updateVinylInfo(long vinylId, UpdateVinylDTO updatedVinyl) {
+        Vinyl vinyl = vinylRepo.getById(vinylId);
+        mapstructMapper.updateVinylDTOToVinyl(updatedVinyl, vinyl);
+        if(vinyl == null) {
+            throw new RequestDeniedException("No vinyl with id: " + vinylId);
+        } else {
+            vinylRepo.save(vinyl);
+            return true;
+        }
     }
 
     @Override
     public boolean deleteVinyl(long vinylId) {
+        vinylRepo.deleteById(vinylId);
+        if(!vinylRepo.findById(vinylId).isPresent())
+            return true;
         return false;
     }
 }
