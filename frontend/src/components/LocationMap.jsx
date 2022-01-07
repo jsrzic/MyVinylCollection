28d12a3233c56dfaf6da175b55663e423641eb77
Lucija Aleksić <lucija.aleksic@fer.hr>
2022-01-07 18:08:58 +0100

    Small fixes

--- frontend/src/components/LocationMap.jsx
+++ frontend/src/components/LocationMap.jsx
@@ -1,18 +1,34 @@
 import React     from "react";
 
 import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import {Typography} from "@mui/material";
+import {ThemeContext} from "../util/utils";
 
 function LocationMap({lat, lng, setLat, setLng, editing}) {
+    const { theme } = React.useContext(ThemeContext);
+    const outline = theme.palette.mode === "dark" ? "rgba(255, 255, 255, 0.5)" : "rgba(0, 0, 0, 0.38)";
+
 
     return (
+        <>
+            <Typography
+                style={{
+                    fontSize: "12px",
+                    fontWeight: "400",
+                    color: outline,
+                    marginTop: "1rem",
+                    paddingLeft: "13px"
+                }}
+            >Location</Typography>
         <MapContainer
             center={[lat, lng]}
             style={{
                 width: "100%",
                 height: 300,
                 margin: "auto",
-                marginTop: "1rem",
-                borderRadius: 10,
+                borderRadius: "5px",
+                border: `1px ${outline} solid`,
+                zIndex: 0
             }}
             zoom={13}
             scrollWheelZoom={false}>
@@ -24,6 +40,7 @@ function LocationMap({lat, lng, setLat, setLng, editing}) {
                 setLat={setLat} setLng={setLng}
             />
         </MapContainer>
+        </>
     )
 }
 
