8c0c4f7e3ce24e90463bc829ccaa6079ee4a68cb
Lucija Aleksić <lucija.aleksic@fer.hr>
2022-01-14 16:50:53 +0100

    Guest view, vinyl info fix

--- frontend/src/components/HomePageAds.jsx
+++ frontend/src/components/HomePageAds.jsx
@@ -1,17 +1,33 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import AdCard from "./AdCard";
 
-function HomePageAds({exchangeAds, saleAds}) {
+function HomePageAds({ exchangeAds, saleAds }) {
   const containerGridStyle = {
     display: "grid",
     gridTemplateColumns: "repeat(5, 1fr)",
-    gap: "2rem"
+    gap: "2rem",
   };
 
   return (
     <div style={containerGridStyle}>
-      {saleAds.map(ad => <AdCard username={ad.username} price={ad.saleAd.price} name={ad.saleAd.vinyl.album} isSale id={ad.saleAd.id}/>)}
-      {exchangeAds.map(ad => <AdCard username={ad.username}  name={ad.exchangeAd.vinyl.album} id={ad.exchangeAd.id}/>)}
+      {saleAds.map((ad) => (
+        <AdCard
+          username={ad.username}
+          price={ad.saleAd.price}
+          name={ad.saleAd.vinyl.album}
+          isSale
+          id={ad.saleAd.id}
+          ad={ad}
+        />
+      ))}
+      {exchangeAds.map((ad) => (
+        <AdCard
+          username={ad.username}
+          name={ad.exchangeAd.vinyl.album}
+          id={ad.exchangeAd.id}
+          ad={ad}
+        />
+      ))}
     </div>
   );
 }
